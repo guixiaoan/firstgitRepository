9fd94e89ac3e2699f904a2300ebee7607c558891
gengdd <gdd1009x@163.com>
2020-07-12 12:07:48 +0800

    这只是一个测试类

---
+++ src/com/guixiaoan/test/Person.java
@@ -0,0 +1,11 @@
+package com.guixiaoan.test;
+
+public class Person {
+    private String name ;
+    private int age ;
+    private Double height;
+
+    public static void main(String[] args) {
+        System.out.println("这只是一个简单的test!");
+    }
+}
