5f1cccd49078746c236a777dde565f3539b7ec96
gengdd <gdd1009x@163.com>
2020-07-12 12:11:18 +0800

    增加了一个属性

--- src/com/guixiaoan/test/Person.java
+++ src/com/guixiaoan/test/Person.java
@@ -5,6 +5,7 @@ public class Person {
     private int age ;
     private Double height;
 
+    private String class1 ;
     public static void main(String[] args) {
         System.out.println("这只是一个简单的test!");
     }
